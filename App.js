65d8b82b3b095465cebb7a4d4cafe85bcc2eb61b
Gladys Tebeila <Gladys.Tebeila@gijima.com>
2022-09-19 11:15:13 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+
+const image = { uri: "./images/pic.jpg" };
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.card}>Birthday Card</Text>
+      <Image 
+      style={styles.img}
+      source={require('./images/pic.jpg')}
+      />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <View style={styles.body}>
+        <Text>
+          HAPPY Birthday
+          Never forget age means nothing withiout the minds
+          
+          </Text>
+      </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 20,
+    backgroundColor:'purple',
     alignItems: 'center',
     justifyContent: 'center',
+  },image: {
+    flex: 1,
+    justifyContent: "center",
   },
+  card: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: -200,
+    marginLeft: '15%',
+    marginRight: '15%',
+  },
+  img: {
+    marginTop: 100,
+    height: 200,
+    width: 200,
+  }
 });
+
